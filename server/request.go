212fd7d4f3ccf92d3465526cf70d0861b6c03282
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-03-13 11:11:49 -0800

    introduce DecodeStrict decoder

--- server/request.go
+++ server/request.go
@@ -9,7 +9,19 @@ import (
 // body is decoded into the provided value.
 func Decode(r *http.Request, val interface{}) error {
 	decoder := json.NewDecoder(r.Body)
-	// decoder.DisallowUnknownFields()
+	if err := decoder.Decode(val); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DecodeStrict reads the body of an HTTP request looking for a JSON document. The
+// body is decoded into the provided value.
+// DecodeStrict will return an error if the JSON document contains fields that
+// are not defined in the provided value.
+func DecodeStrict(r *http.Request, val interface{}) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
 	if err := decoder.Decode(val); err != nil {
 		return err
 	}
