9aaeda1b1569e41a37c1c3ad34fb02dec05c0dcb
Hayden <64056131+hay-kot@users.noreply.github.com>
2023-04-09 10:16:33 -0800

    deprecate middleware

--- server/middleware.go
+++ server/middleware.go
@@ -8,6 +8,8 @@ import (
 // StripTrailingSlash is a middleware that will strip trailing slashes from the request path.
 //
 // Example: /api/v1/ -> /api/v1
+//
+// Deprecated: Use alternative middleware from Chi or Gorilla.
 func StripTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
